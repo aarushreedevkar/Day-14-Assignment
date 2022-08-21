b13f4a13a7121f3d850c629db2622f928eda5f7f
jyoti <jyotidhage10@gmail.com>
2022-08-21 15:54:27 +0530

    [Aaradhya]Add:Implement Stack Using Linked List

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackAndQueue
+{
+     class Program
+    {
+        public static void Main(string[] args)
+        {
+            Struct_Node.push(70,30,56);
+        }
+    }
+}
---
+++ StructNode.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public static class StructNode
+    {
+        int data;
+        struct Node * Next;
+           
+         void linkedList Traversal(struct Node * ptr) {
+           while (Ptr ! Null ) {
+            console.writeline("Element: 'ptr data);
+            ptr = IntPtr - > Next;
+            }
+}
+;            
+            
+            }
+            
+            
+            public static  void push (Struct_Node * top,int x)
+        {
+          struct Node *top = null;
+            top = push(top,70);
+            top = push(top, 30);
+            top = push(top, 56);
+linkedListTraversal(top);
+
+        }
+        int isEmpty(Struct Node * top)
+    {
+        if top == Null{
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+        int isFull(struct Node * top){
+        Struct Node * p =(struct Node *)malloc(size of (struct Node))
+        if(n == Null){
+        return 1;
+        else {
+        retuurn 0;
+
+    }
+    
+    Struct Node * push( struct Node * top ,int x){
+    if (isFull(top))
+    {
+        Console.WriteLine("Stack Overflow");
+    }
+    else
+    {
+        struct Node * n= (struct Node * )malloc ( size of (struct Node));
+         n->data = x;
+         n->Next = top;
+
+          top = n;
+          return top
+
+}
+}
+
+
+
+
+
+
+
+
+
+    }
+}
